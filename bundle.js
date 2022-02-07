90668200c09de6ff8b204483c15ef9af3cc2dfc5
WalidK <walidkhaffou@gmail.com>
2022-02-07 00:15:57 +0000

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -212,8 +212,9 @@ window.extend = function() {
 		ding.play();
 		document.getElementById("button").disabled = false;
 		document.getElementById("loader-inner").style.animation = "none";
-		document.getElementById("inbox").value = response.completions[1].encoding;
-		document.getElementById("button").click();
+		alert( oldDuration );
+		//document.getElementById("inbox").value = response.completions[1].encoding;
+		//document.getElementById("button").click();
 	}).catch(error => {
 		ding.play();
 		document.getElementById("button").disabled = false;
